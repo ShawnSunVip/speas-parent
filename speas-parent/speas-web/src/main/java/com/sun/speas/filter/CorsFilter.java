25eb6b08a71c82a8d687204715aa2a0d9bbde6c4
ShawnSunVip <824390715@qq.com>
2020-11-19 09:21:42 +0800

    add cors

---
+++ speas-parent/speas-web/src/main/java/com/sun/speas/filter/CorsFilter.java
@@ -0,0 +1,52 @@
+package com.sun.speas.filter;
+
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 跨域设置
+ * @author sunxiang
+ * @date 2020-11-19 8:33
+ **/
+@Component
+@ServletComponentScan
+@WebFilter(urlPatterns = "/*",filterName = "CorsFilter" )
+public class CorsFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        //跨域
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        //跨域 Header
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        //指定本次预检请求的有效期，单位为秒
+        response.setHeader("Access-Control-Max-Age", "1728000");
+        //允许跨域请求中携带cookie
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        //如果存在自定义的header参数，需要在此处添加，逗号分隔
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
